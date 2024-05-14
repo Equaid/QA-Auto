eaef2c3df4deaf6f356c742f046b624ab19f871a
Oleksandr Melenchuk <sageinc96@gmail.com>
2024-05-14 20:28:32 +0300

    Created Test

---
+++ tests/api/test_api.py
@@ -0,0 +1,12 @@
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+def test_name(user):
+    assert user.name == 'Sergii'
+
+
+def test_second_name(user):
+    assert user.second_name == 'Butenko'
